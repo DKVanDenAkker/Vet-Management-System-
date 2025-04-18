10e2c42d8a22a55f8825ab3242fa33ad6b35515d
Mongkhon Chumpoothep <6731503027@lamduan.mfu.ac.th>
2025-04-18 18:48:57 +0700

    Update Amphibian.java

--- Amphibian.java
+++ Amphibian.java
@@ -1,7 +1,8 @@
 public class Amphibian extends Animal {
-
+//The Amphibian class inherits from the Animal class. This means it has all the properties and behaviors of Animal, but can also add or override specific behaviors.
+    
     public Amphibian(String name, int age, String type, String condition, String owner, boolean needsAppointment, String appointmentDate) {
-        super(name, age, type, condition, owner, needsAppointment, appointmentDate);
+        // super(name, age, type, condition, owner, needsAppointment, appointmentDate);//รับพารามิเตอร์ครบทั้งหมด แล้วส่งค่าทั้งหมดไปยังคอนสตรัคเตอร์ของคลาสแม่ (super) ซึ่งคือ Animal
     }
 
     public Amphibian(String name, int age, String type, String condition, String owner) {
@@ -10,7 +11,7 @@ public class Amphibian extends Animal {
 
     @Override
     public void displayInfo() {
-        System.out.println("Species: Amphibian");
+        System.out.println("Species: Amphibian"); //method is overridden to display details specific to an amphibian, including its name, age, type, condition, etc.
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Type: " + type);
